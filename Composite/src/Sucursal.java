93f052f91de028af5220c789e91589aedcfa497b
Daniel <despinalm@unal.edu.co>
2021-04-16 17:21:21 -0500

    Composite getChildren

--- Composite/src/Sucursal.java
+++ Composite/src/Sucursal.java
@@ -5,22 +5,23 @@ public class Sucursal implements Departamento {
 	private  Integer id;
 	private String nombre;
 	private List<Departamento> DepartamentoHijos;
-	
 	public Sucursal(Integer id, String nombre) {
 		this.id = id;
 		this.nombre = nombre;
 		this.DepartamentoHijos = new ArrayList<>();
 	}
-	
 	public void imprimirNombreDepartamento() {
 		DepartamentoHijos.forEach(Departamento::imprimirNombreDepartamento);
 	}
-	
 	public void addDepartamento(Departamento departamento) {
 		DepartamentoHijos.add(departamento);
 	}
-	
 	public void removeDepartamento(Departamento departamento) {
 		DepartamentoHijos.remove(departamento);
 	}
+	public List<Departamento> getChildren() {
+		return this.DepartamentoHijos;
+	}
 }
+
+
